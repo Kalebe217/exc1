a27de288f88405095c9d298508f66c8fae4b7d88
20221TIIMI0327 <kalebe21.55@gmail.com>
2024-10-24 12:12:32 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    echo "<img src = 'https://picsum.photos/500/600' alt='imagem gerada'>";
+    echo "<br>";
+    echo "imagem";
+
+
+
+
+
+
+
+     // URL's absolutas
+     echo "<h2>URLs Absolutas</h2>";
+     echo "<ul>";
+     echo "<li><a href='https://www.w3.org/'>W3C</a></li>";
+     echo "<li><a href='https://www.google.com/'>Google</a></li>";
+     echo "</ul>";
+
+     // URL's relativas
+     echo "<h2>URLs Relativas</h2>";
+     echo "<ul>";
+     echo "<li><a href='img.jfif'>HTML Images</a></li>";
+     echo "<li><a href='img2.jfif'>CSS Tutorial</a></li>";
+     echo "</ul>";
+    ?>
+</body>
+</html>
